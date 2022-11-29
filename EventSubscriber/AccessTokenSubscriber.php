c3876b007a73cf713f9b7d6a881b040383826b22
kurozumi <info@a-zumi.net>
2022-11-29 13:04:55 +0900

    fix

--- EventSubscriber/AccessTokenSubscriber.php
+++ EventSubscriber/AccessTokenSubscriber.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * This file is part of Auth0
+ *
+ * Copyright(c) Akira Kurozumi <info@a-zumi.net>
+ *
+ *  https://a-zumi.net
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Plugin\Auth0\EventSubscriber;
 
 use Eccube\Request\Context;
