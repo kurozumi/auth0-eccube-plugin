47c493429f22bdb0b1ca3c961e438dce0cd6b0e9
kurozumi <info@a-zumi.net>
2024-06-17 14:04:55 +0900

    fix workflows

---
+++ Tests/DependencyInjection/Auth0ExtensionTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Plugin\Auth0\Tests\DependencyInjection;
+
+use Eccube\Tests\EccubeTestCase;
+use Plugin\Auth0\DependencyInjection\Auth0Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class Auth0ExtensionTest extends EccubeTestCase
+{
+    public function testプラグインが無効化されていたらAuth0Authenticatorは追加されない()
+    {
+        $container = $this->createMock(ContainerBuilder::class);
+        $container->expects($this->once())
+            ->method('getParameter')
+            ->willReturn([]);
+
+        $extension = new Auth0Extension();
+        $extension->prepend($container);
+    }
+}
